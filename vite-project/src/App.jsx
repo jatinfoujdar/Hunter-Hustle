f86ca922e25d5711af2113c0a2194885aef4bc6d
Jatin Foujdar <jatin321001@gmail.com>
2024-02-09 02:03:02 +0530

    landing page commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,36 @@
-import WordQuest from "./components/WordQuest/wordQuest"
+import React from 'react'
 
-
-
-function App() {
+const App = () => {
   return (
-    <WordQuest/>
+
+<div class="flex flex-col items-center justify-center h-screen bg-[#E6FCFF]">
+  <div class="text-4xl font-bold text-[#008080] my-10">Welcome to Hunter Hustle App</div>
+<div class="grid grid-cols-2 gap-4 w-3/4">
+  <a class="flex flex-col items-center justify-center py-4 px-6 rounded-xl bg-[#FFCCE6] text-[#800000] font-semibold text-lg hover:bg-[#FF99CC] transition-colors duration-300" href="#" rel="ugc">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-10 w-10 mb-2"><path d="M5 12h14"></path><path d="M12 5v14"></path>
+    </svg>
+ Word Quest
+</a>
+<a class="flex flex-col items-center justify-center py-4 px-6 rounded-xl bg-[#E6E6FA] text-[#000080] font-semibold text-lg hover:bg-[#CCCCFF] transition-colors duration-300" href="#" rel="ugc">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-10 w-10 mb-2">
+    <path d="M5 12h14"></path>
+    </svg>
+    Decryption Challenge
+</a><a class="flex flex-col items-center justify-center py-4 px-6 rounded-xl bg-[#E6FAE6] text-[#008000] font-semibold text-lg hover:bg-[#CCFFCC] transition-colors duration-300" href="#" rel="ugc">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-10 w-10 mb-2">
+    <circle cx="12" cy="6" r="1"></circle><line x1="5" x2="19" y1="12" y2="12"></line><circle cx="12" cy="18" r="1">
+      </circle></svg>
+      Code Trivia
+</a><a class="flex flex-col items-center justify-center py-4 px-6 rounded-xl bg-[#FAE6E6] text-[#800000] font-semibold text-lg hover:bg-[#FFCCCC] transition-colors duration-300" href="#" rel="ugc">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-10 w-10 mb-2">
+    <circle cx="12" cy="6" r="1">
+      </circle><line x1="5" x2="19" y1="12" y2="12"></line>
+      <circle cx="12" cy="18" r="1"></circle>
+      </svg>
+      Riddle Reveal
+</a>
+</div>
+</div>
   )
 }
 
